4ba87943124a22431e7b019d1e59824dd847489a
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2023-12-08 17:17:58 +0530

    updated db

--- utils/database.ts
+++ utils/database.ts
@@ -11,7 +11,9 @@ export const connectToDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI || "");
+    await mongoose.connect(
+      "mongodb+srv://rebel:7017614925@cluster0.ihexdsa.mongodb.net/energymonitor?retryWrites=true&w=majority"
+    );
 
     isConnected = true;
 
